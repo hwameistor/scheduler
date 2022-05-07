c0e4411ae93e35cfb41808ccf4141a157b39c3c1
123 <123>
2022-05-07 11:15:11 +0800

    add e2e test

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -59,6 +59,6 @@ do
 done
 ##
 sed -i '/.*ghcr.io*/c\hwameistorImageRegistry: 10.6.170.180' test/helm-charts/charts/hwameistor/values.yaml
-sed -i '/local-storage/{n;d}' test/helm-charts/charts/hwameistor/values.yaml
-sed -i '/local-storage/a \ \ \ \ tag: 99.9-dev' test/helm-charts/charts/hwameistor/values.yaml
+sed -i '/hwameistor\/scheduler/{n;d}' test/helm-charts/charts/hwameistor/values.yaml
+sed -i '/hwameistor\/scheduler/a \ \ \ \ tag: 99.9-dev' test/helm-charts/charts/hwameistor/values.yaml
 ginkgo --fail-fast --label-filter=${E2E_TESTING_LEVEL} test/e2e
