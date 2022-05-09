fe472b34e55cab908a43d1157730d68f7c9bc90d
123 <123>
2022-05-09 16:16:29 +0800

    fix

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -60,5 +60,5 @@ done
 ##
 sed -i '/.*ghcr.io*/c\hwameistorImageRegistry: 10.6.170.180' test/helm-charts/charts/hwameistor/values.yaml
 sed -i '/hwameistor\/scheduler/{n;d}' test/helm-charts/charts/hwameistor/values.yaml
-sed -i '/hwameistor\/scheduler/a \ \ \ \ tag: 99.9-dev' test/helm-charts/charts/hwameistor/values.yaml
+sed -i '/hwameistor\/scheduler/a \ \ tag: 99.9-dev' test/helm-charts/charts/hwameistor/values.yaml
 ginkgo --fail-fast --label-filter=${E2E_TESTING_LEVEL} test/e2e
