153bef519d2682f64abe319086432a4d64c2e9ab
123 <123>
2022-06-20 09:21:33 +0800

    fix

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -1,37 +1,4 @@
-#! /usr/bin/env bash
-# simple scripts mng machine
-# link hosts
-export GOVC_INSECURE=1
-export GOVC_RESOURCE_POOL="e2e"
-export hosts="fupan-e2e-k8s-master fupan-e2e-k8s-node1 fupan-e2e-k8s-node2"
-export snapshot="e2etest"
-# for h in hosts; do govc vm.power -off -force $h; done
-# for h in hosts; do govc snapshot.revert -vm $h "机器配置2"; done
-# for h in hosts; do govc vm.power -on -force $h; done
 
-# govc vm.info $hosts[0].Power state
-# govc find . -type m -runtime.powerState poweredOn
-# govc find . -type m -runtime.powerState poweredOn | xargs govc vm.info
-# govc vm.info $hosts
-
-for h in $hosts; do
-  if [[ `govc vm.info $h | grep poweredOn | wc -l` -eq 1 ]]; then
-    govc vm.power -off -force $h
-    echo -e "\033[35m === $h has been down === \033[0m"
-  fi
-
-  govc snapshot.revert -vm $h $snapshot
-  echo -e "\033[35m === $h reverted to snapshot: `govc snapshot.tree -vm $h -C -D -i -d` === \033[0m"
-
-  govc vm.power -on $h
-  echo -e "\033[35m === $h: power turned on === \033[0m"
-done
-
-echo -e "\033[35m === task will end in 1m 30s === \033[0m"
-for i in `seq 1 15`; do
-  echo -e "\033[35m === `date  '+%Y-%m-%d %H:%M:%S'` === \033[0m"
-  sleep 6s
-done
 git clone https://github.com/hwameistor/helm-charts.git test/helm-charts
 cat test/helm-charts/charts/hwameistor/values.yaml | while read line
 ##
@@ -61,6 +28,7 @@ done
 date=$(date +%Y%m%d%H%M)
 docker tag $ImageRegistry/hwameistor/scheduler:99.9-dev $ImageRegistry/hwameistor/scheduler:$date
 docker push $ImageRegistry/hwameistor/scheduler:$date
+echo "docker push $ImageRegistry/hwameistor/scheduler:$date"
 sed -i '/.*ghcr.io*/c\hwameistorImageRegistry: 10.6.170.180' test/helm-charts/charts/hwameistor/values.yaml
 sed -i '/hwameistor\/scheduler/{n;d}' test/helm-charts/charts/hwameistor/values.yaml
 sed -i '/hwameistor\/scheduler/a \ \ tag: 99.9-dev' test/helm-charts/charts/hwameistor/values.yaml
